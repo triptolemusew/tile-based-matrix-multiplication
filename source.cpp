da6b4c09b991226531f5a18a7fd9f4bc14e63f5a
Faris Danial <triptolemusew@users.noreply.github.com>
2017-02-15 20:02:33 +0800

    Fixed irregular matrix

--- source.cpp
+++ source.cpp
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <memory.h>
@@ -20,6 +21,7 @@ FILE *fileA1;
 FILE *fileA2;
 FILE *fileB1;
 FILE *fileB2;
+FILE *fileC;
 
 void *threadFunc(void *pArg){
 	int tID = *((int*)pArg);
@@ -64,7 +66,9 @@ int **allocate_2d_array(int rows, int cols){
 	return result;
 }
 
-void deallocate_2d_array(int **array){
+void deallocate_2d_array(int **array, int rows){
+	for (int i = 1; i < rows; i++)
+		array[i] = NULL;
 	free(array[0]);
 	free(array);
 }
@@ -75,7 +79,6 @@ int main(int argc, char* argv[])
 	InitializeTimer();
 	int myRank, totalProcs;
 	int numPerProcs, numPerProcsRemain;
-	FILE *fileC;
 
 	int i = 0, j = 0, k = 0;
 	pthread_t pThreads[NUM_THREADS];
@@ -145,30 +148,35 @@ int main(int argc, char* argv[])
 
 	//Variables for each process that handles num of processes in both matrix A & B
 	int sizeRowPerProcsA = g_rowA / 2;
+	int sizeRowPerProcsARemain = g_rowA % 2;
 	int sizeColPerProcsA = g_colA / 2;
 	int sizeRowPerProcsB = g_rowB / 2;
 	int sizeColPerProcsB = g_colB / 2;
+	int sizeColPerProcsBRemain = g_colB % 2;
 	int sizeRowPerPartC = g_rowA / 2;
+	int sizeRowPerPartCRemain = g_rowA % 2;
 	int sizeColPerPartC = g_colB / 2;
+	int sizeColPerPartCRemain = g_colB % 2;
 
 	rowC = g_rowA;
 	colC = g_colB;
-	if (myRank == 0){
-		startTime = GetCustomCurrentTime();
-	}
-	if (myRank != 0){
-		matrixA = allocate_2d_array(sizeRowPerProcsA, g_colA);
-		matrixB = allocate_2d_array(g_rowB, sizeColPerProcsB);
-	}
-	cpart = allocate_2d_array(sizeRowPerPartC, sizeColPerPartC);
-
+	
 	//2. Storing 2D matrix into 1D and send to each node
 	switch (myRank){
 	case 0: {
+		cpart = allocate_2d_array(sizeRowPerPartC, sizeColPerPartC);
 		temp_1dA1 = (int *)malloc(sizeRowPerProcsA * g_colA * sizeof(int));
-		temp_1dA2 = (int *)malloc(sizeRowPerProcsA * g_colA * sizeof(int));
+		if(sizeRowPerProcsARemain != 0){
+			temp_1dA2 = (int *)malloc((sizeRowPerProcsA + sizeRowPerProcsARemain) * g_colA * sizeof(int));
+		} else{
+			temp_1dA2 = (int *)malloc(sizeRowPerProcsA * g_colA * sizeof(int));
+		}
 		temp_1dB1 = (int *)malloc(g_rowB * sizeColPerProcsB * sizeof(int));
-		temp_1dB2 = (int *)malloc(g_rowB * sizeColPerProcsB * sizeof(int));
+		if(sizeColPerProcsBRemain != 0){
+			temp_1dB2 = (int *)malloc(g_rowB * (sizeColPerProcsB + sizeColPerProcsBRemain) * sizeof(int));
+		} else {
+			temp_1dB2 = (int *)malloc(g_rowB * sizeColPerProcsB * sizeof(int));
+		}
 		matrixC = allocate_2d_array(rowC, colC);
 		int l = 0;
 		for (int i = 0; i < g_rowB; i++){
@@ -181,7 +189,7 @@ int main(int argc, char* argv[])
 		l = 0;
 		offsetBCol += sizeColPerProcsB;
 		for (int i = 0; i < g_rowB; i++){
-			for (int j = 0; j < sizeColPerProcsB; j++){
+			for (int j = 0; j < sizeColPerProcsB + sizeColPerProcsBRemain; j++){
 				temp_1dB2[l] = g_MatrixB[i][j + offsetBCol];
 				l++;
 			}
@@ -198,7 +206,7 @@ int main(int argc, char* argv[])
 
 		l = 0;
 		offsetBRow += sizeRowPerProcsA;
-		for (int i = 0; i < sizeRowPerProcsA; i++){
+		for (int i = 0; i < sizeRowPerProcsA + sizeRowPerProcsARemain; i++){
 			for (int j = 0; j < g_colA; j++){
 				temp_1dA2[l] = g_MatrixA[i + offsetBRow][j];
 				l++;
@@ -213,21 +221,23 @@ int main(int argc, char* argv[])
 		for (int i = 1; i < totalProcs; i++){
 			if (i == 1){
 				MPI_Send(&temp_1dA1[0], sizeRowPerProcsA * g_colA, MPI_INT, i, 0, MPI_COMM_WORLD);
-				MPI_Send(&temp_1dB2[0], g_rowB * sizeColPerProcsB, MPI_INT, i, 0, MPI_COMM_WORLD);
+				MPI_Send(&temp_1dB2[0], g_rowB * (sizeColPerProcsBRemain+ sizeColPerProcsB), MPI_INT, i, 0, MPI_COMM_WORLD);
 			}
 			else if (i == 2){
-				MPI_Send(&temp_1dA2[0], sizeRowPerProcsA * g_colA, MPI_INT, i, 0, MPI_COMM_WORLD);
+				MPI_Send(&temp_1dA2[0], (sizeRowPerProcsARemain+sizeRowPerProcsA) * g_colA, MPI_INT, i, 0, MPI_COMM_WORLD);
 				MPI_Send(&temp_1dB1[0], g_rowB * sizeColPerProcsB, MPI_INT, i, 0, MPI_COMM_WORLD);
 			}
 			else if (i == 3){
-				MPI_Send(&temp_1dA2[0], sizeRowPerProcsA * g_colA, MPI_INT, i, 0, MPI_COMM_WORLD);
-				MPI_Send(&temp_1dB2[0], g_rowB * sizeColPerProcsB, MPI_INT, i, 0, MPI_COMM_WORLD);
+				MPI_Send(&temp_1dA2[0], (sizeRowPerProcsARemain + sizeRowPerProcsA) * g_colA, MPI_INT, i, 0, MPI_COMM_WORLD);
+				MPI_Send(&temp_1dB2[0], g_rowB * (sizeColPerProcsBRemain+ sizeColPerProcsB), MPI_INT, i, 0, MPI_COMM_WORLD);
 			}
 		}
 
 		//Matrix Multiplication for root node
 		for (int i = 0; i < sizeRowPerPartC; i++){
-			memset(cpart[i], 0, (sizeColPerPartC * sizeof(int)));
+			for (int j = 0; j < sizeColPerPartC; j++){
+				cpart[i][j] = 0.0e0;
+			}
 		}
 		for (int i = 0; i < sizeRowPerPartC; i++){
 			for (int k = 0; k < g_colA; k++){
@@ -236,18 +246,22 @@ int main(int argc, char* argv[])
 				}
 			}
 		}
+		
 		break;
 	}
 	default: {
 		//3. Calculations for each partition of Matrix C
 		if (myRank == 1){
+			cpart = allocate_2d_array(sizeRowPerPartC, sizeColPerPartC + sizeColPerProcsBRemain);
+			matrixA = allocate_2d_array(sizeRowPerProcsA, g_colA);
+			matrixB = allocate_2d_array(g_rowB, sizeColPerProcsB + sizeColPerProcsBRemain);
 			temp_1dA1 = (int *)malloc(sizeRowPerProcsA * g_colA * sizeof(int));
-			temp_1dB2 = (int *)malloc(g_rowB * sizeColPerProcsB * sizeof(int));
+			temp_1dB2 = (int *)malloc(g_rowB * (sizeColPerProcsBRemain +sizeColPerProcsB) * sizeof(int));
 			MPI_Recv(&temp_1dA1[0], sizeRowPerProcsA * g_colA, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-			MPI_Recv(&temp_1dB2[0], g_rowB * sizeColPerProcsB, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+			MPI_Recv(&temp_1dB2[0], g_rowB * (sizeColPerProcsBRemain+sizeColPerProcsB), MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
 			int l = 0;
 			for (int i = 0; i < g_rowB; i++){
-				for (int j = 0; j < sizeColPerProcsB; j++){
+				for (int j = 0; j < sizeColPerProcsB + sizeColPerProcsBRemain; j++){
 					matrixB[i][j] = temp_1dB2[l];
 					l++;
 				}
@@ -259,26 +273,30 @@ int main(int argc, char* argv[])
 					l++;
 				}
 			}
-
 			for (int i = 0; i < sizeRowPerPartC; i++){
-				memset(cpart[i], 0, (sizeColPerPartC * sizeof(int)));
+				for (int j = 0; j < sizeColPerPartC + sizeColPerProcsBRemain; j++){
+					cpart[i][j] = 0.0e0;
+				}
 			}
 
 			for (int i = 0; i < sizeRowPerPartC; i++){
 				for (int k = 0; k < g_colA; k++){
-					for (int j = 0; j < sizeColPerPartC; j++){
+					for (int j = 0; j < sizeColPerPartC + sizeColPerProcsBRemain; j++){
 						cpart[i][j] += matrixA[i][k] * matrixB[k][j];
 					}
 				}
 			}
 			free(temp_1dA1);
 			free(temp_1dB2);
-			MPI_Send(&cpart[0][0], sizeRowPerPartC * sizeColPerPartC, MPI_INT, 0, 0, MPI_COMM_WORLD);
+			MPI_Send(&cpart[0][0], sizeRowPerPartC * (sizeColPerPartC + sizeColPerProcsBRemain), MPI_INT, 0, 0, MPI_COMM_WORLD);
 		}
 		else if (myRank == 2){
-			temp_1dA2 = (int *)malloc(sizeRowPerProcsA * g_colA * sizeof(int));
+			cpart = allocate_2d_array(sizeRowPerPartC + sizeRowPerProcsARemain, sizeColPerPartC);
+			matrixA = allocate_2d_array(sizeRowPerProcsA + sizeRowPerProcsARemain, g_colA);
+			matrixB = allocate_2d_array(g_rowB, sizeColPerProcsB);
+			temp_1dA2 = (int *)malloc((sizeRowPerProcsARemain+sizeRowPerProcsA) * g_colA * sizeof(int));
 			temp_1dB1 = (int *)malloc(g_rowB * sizeColPerProcsB * sizeof(int));
-			MPI_Recv(&temp_1dA2[0], sizeRowPerProcsA * g_colA, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+			MPI_Recv(&temp_1dA2[0], (sizeRowPerProcsARemain+sizeRowPerProcsA) * g_colA, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
 			MPI_Recv(&temp_1dB1[0], g_rowB * sizeColPerProcsB, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
 			int l = 0;
 			for (int i = 0; i < g_rowB; i++){
@@ -288,18 +306,19 @@ int main(int argc, char* argv[])
 				}
 			}
 			l = 0;
-			for (int i = 0; i < sizeRowPerProcsA; i++){
+			for (int i = 0; i < sizeRowPerProcsA + sizeRowPerProcsARemain; i++){
 				for (int j = 0; j < g_colA; j++){
 					matrixA[i][j] = temp_1dA2[l];
 					l++;
 				}
 			}
-
-			for (int i = 0; i < sizeRowPerPartC; i++){
-				memset(cpart[i], 0, (sizeColPerPartC * sizeof(int)));
+			for (int i = 0; i < sizeRowPerPartC + sizeRowPerProcsARemain; i++){
+				for (int j = 0; j < sizeColPerPartC; j++){
+					cpart[i][j] = 0.0e0;
+				}
 			}
 
-			for (int i = 0; i < sizeRowPerPartC; i++){
+			for (int i = 0; i < sizeRowPerPartC + sizeRowPerProcsARemain; i++){
 				for (int k = 0; k < g_colA; k++){
 					for (int j = 0; j < sizeColPerPartC; j++){
 						cpart[i][j] += matrixA[i][k] * matrixB[k][j];
@@ -308,42 +327,46 @@ int main(int argc, char* argv[])
 			}
 			free(temp_1dA2);
 			free(temp_1dB1);
-			MPI_Send(&cpart[0][0], sizeRowPerPartC * sizeColPerPartC, MPI_INT, 0, 0, MPI_COMM_WORLD);
+			MPI_Send(&cpart[0][0], (sizeRowPerProcsARemain+sizeRowPerPartC) * sizeColPerPartC, MPI_INT, 0, 0, MPI_COMM_WORLD);
 		}
 		else if (myRank == 3){
-			temp_1dA2 = (int *)malloc(sizeRowPerProcsA * g_colA * sizeof(int));
-			temp_1dB2 = (int *)malloc(g_rowB * sizeColPerProcsB * sizeof(int));
-			MPI_Recv(&temp_1dA2[0], sizeRowPerProcsA * g_colA, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-			MPI_Recv(&temp_1dB2[0], g_rowB * sizeColPerProcsB, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+			cpart = allocate_2d_array(sizeRowPerPartC + sizeRowPerProcsARemain, sizeColPerPartC + sizeColPerProcsBRemain);
+			matrixA = allocate_2d_array(sizeRowPerProcsA + sizeRowPerProcsARemain, g_colA);
+			matrixB = allocate_2d_array(g_rowB, sizeColPerProcsB + sizeColPerProcsBRemain);
+			temp_1dA2 = (int *)malloc((sizeRowPerProcsARemain+sizeRowPerProcsA) * g_colA * sizeof(int));
+			temp_1dB2 = (int *)malloc(g_rowB * (sizeColPerProcsBRemain+sizeColPerProcsB) * sizeof(int));
+			MPI_Recv(&temp_1dA2[0], (sizeRowPerProcsARemain+sizeRowPerProcsA) * g_colA, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+			MPI_Recv(&temp_1dB2[0], g_rowB * (sizeColPerProcsBRemain+sizeColPerProcsB), MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
 			int l = 0;
 			for (int i = 0; i < g_rowB; i++){
-				for (int j = 0; j < sizeColPerProcsB; j++){
+				for (int j = 0; j < sizeColPerProcsB + sizeColPerProcsBRemain; j++){
 					matrixB[i][j] = temp_1dB2[l];
 					l++;
 				}
 			}
 			l = 0;
-			for (int i = 0; i < sizeRowPerProcsA; i++){
+			for (int i = 0; i < sizeRowPerProcsA + sizeRowPerProcsARemain; i++){
 				for (int j = 0; j < g_colA; j++){
 					matrixA[i][j] = temp_1dA2[l];
 					l++;
 				}
 			}
-
-			for (int i = 0; i < sizeRowPerPartC; i++){
-				memset(cpart[i], 0, (sizeColPerPartC * sizeof(int)));
+			for (int i = 0; i < sizeRowPerPartC + sizeRowPerProcsARemain; i++){
+				for (int j = 0; j < sizeColPerPartC + sizeColPerProcsBRemain; j++){
+					cpart[i][j] = 0.0e0;
+				}
 			}
-
-			for (int i = 0; i < sizeRowPerPartC; i++){
+			for (int i = 0; i < sizeRowPerPartC + sizeRowPerProcsARemain; i++){
 				for (int k = 0; k < g_colA; k++){
-					for (int j = 0; j < sizeColPerPartC; j++){
+					for (int j = 0; j < sizeColPerPartC + sizeColPerProcsBRemain; j++){
 						cpart[i][j] += matrixA[i][k] * matrixB[k][j];
 					}
 				}
 			}
+
 			free(temp_1dA2);
 			free(temp_1dB2);
-			MPI_Send(&cpart[0][0], sizeRowPerPartC * sizeColPerPartC, MPI_INT, 0, 0, MPI_COMM_WORLD);
+			MPI_Send(&cpart[0][0], (sizeRowPerPartC + sizeRowPerProcsARemain) * (sizeColPerPartC+sizeColPerProcsBRemain), MPI_INT, 0, 0, MPI_COMM_WORLD);
 			}
 		}
 	break;
@@ -354,67 +377,68 @@ int main(int argc, char* argv[])
 		int offsetRow = 0;
 		int offsetCol = 0;
 
-		for (i = 0; i < sizeRowPerPartC; i++){
-			for (j = 0; j < sizeColPerPartC; j++){
-				matrixC[i][j] = cpart[i][j];
+		int**cpart1 = allocate_2d_array(sizeRowPerPartC, sizeColPerPartC + sizeColPerProcsBRemain);
+		int**cpart2 = allocate_2d_array(sizeRowPerPartC + sizeRowPerProcsARemain, sizeColPerPartC);
+		int**cpart3 = allocate_2d_array(sizeRowPerPartC + sizeRowPerProcsARemain, sizeColPerPartC + sizeColPerProcsBRemain);
+			for (i = 0; i < sizeRowPerPartC; i++){
+				for (j = 0; j < sizeColPerPartC; j++){
+					matrixC[i][j] = cpart[i][j];
+				}
 			}
-		}
-		MPI_Recv(&cpart[0][0], sizeRowPerPartC * sizeColPerPartC, MPI_INT, 1, 0, MPI_COMM_WORLD, &status);
+		MPI_Recv(&cpart1[0][0], sizeRowPerPartC * (sizeColPerPartC + sizeColPerProcsBRemain), MPI_INT, 1, 0, MPI_COMM_WORLD, &status);
 		offsetCol += sizeColPerPartC;
 		for (i = 0; i < sizeRowPerPartC; i++){
-			for (j = 0; j < sizeColPerPartC; j++){
-				matrixC[i][j + offsetCol] = cpart[i][j];
+			for (j = 0; j < sizeColPerPartC + sizeColPerProcsBRemain; j++){
+				matrixC[i][j + offsetCol] = cpart1[i][j];
 			}
 		}
-		MPI_Recv(&cpart[0][0], sizeRowPerPartC * sizeColPerPartC, MPI_INT, 2, 0, MPI_COMM_WORLD, &status);
+		MPI_Recv(&cpart2[0][0], (sizeRowPerProcsARemain+sizeRowPerPartC) * sizeColPerPartC, MPI_INT, 2, 0, MPI_COMM_WORLD, &status);
 		offsetCol = 0;
 		offsetRow += sizeRowPerPartC;
-		for (i = 0; i < sizeRowPerPartC; i++){
+		for (i = 0; i < sizeRowPerPartC + sizeRowPerProcsARemain; i++){
 			for (j = 0; j < sizeColPerPartC; j++){
-				matrixC[i + offsetRow][j] = cpart[i][j];
+				matrixC[i + offsetRow][j] = cpart2[i][j];
 			}
 		}
-		MPI_Recv(&cpart[0][0], sizeRowPerPartC * sizeColPerPartC, MPI_INT, 3, 0, MPI_COMM_WORLD, &status);
+		MPI_Recv(&cpart3[0][0], (sizeRowPerPartC + sizeRowPerProcsARemain) * (sizeColPerPartC+sizeColPerProcsBRemain), MPI_INT, 3, 0, MPI_COMM_WORLD, &status);
 		offsetCol += sizeColPerPartC;
-		for (i = 0; i < sizeRowPerPartC; i++){
-			for (j = 0; j < sizeColPerPartC; j++){
-				matrixC[i + offsetCol][j + offsetRow] = cpart[i][j];
+		for (i = 0; i < sizeRowPerPartC+sizeRowPerProcsARemain; i++){
+			for (j = 0; j < sizeColPerPartC+sizeColPerProcsBRemain; j++){
+				matrixC[i + offsetRow][j + offsetCol] = cpart3[i][j];
 			}
 		}
-	endTime = GetCustomCurrentTime();
+	/*endTime = GetCustomCurrentTime();
 	processTime = endTime - startTime;
 	printf("Process time (ms) for Matrix Multiplication: %lld\n", processTime / 1000);
+	}*/
 	}
-
 	//5. Save the output to "mc.bin"
 	if (myRank == 0){
 		startTime = GetCustomCurrentTime();
-		/*printf("\nFinal Result in Matrix C is: \n");
+		printf("\nFinal Result in Matrix C is: \n");
 			for (int i = 0; i < rowC; i++){
 				for (int j = 0; j < colC; j++){
 					printf("%d\t", matrixC[i][j]);
 				}
 			printf("\n");
-		}*/
-
+		}
 		fileC = fopen("mc.bin", "wb");
 		fwrite(&rowC, sizeof(int), 1, fileC);
 		fwrite(&colC, sizeof(int), 1, fileC);
 		for (int i = 0; i < rowC; i++){
-			fwrite(matrixC[i], sizeof(unsigned long long), colC, fileC);
+			fwrite(matrixC[i], sizeof(int), colC, fileC);
 		}
 		fclose(fileC);
-
 		endTime = GetCustomCurrentTime();
 		processTime = endTime - startTime;
 		printf("Process time (ms) for Writing output to file is: %lld\n", processTime / 1000);
 		printf("Results done printed!");
 	}
 	
-	// 6. Cleaning up!
+	// 5. Cleaning up!
 	if (myRank == 0){
-		deallocate_2d_array(matrixC);
-		deallocate_2d_array(cpart);
+		deallocate_2d_array(matrixC, rowC);
+		deallocate_2d_array(cpart, sizeRowPerPartC);
 		free(temp_1dA1);
 		free(temp_1dA2);
 		free(temp_1dB1);
@@ -422,9 +446,9 @@ int main(int argc, char* argv[])
 		printf("Matrix Multiplication using Multi-Dimension Arrays - Done\n");
 	}
 	else {
-		deallocate_2d_array(matrixA);
-		deallocate_2d_array(matrixB);
-		deallocate_2d_array(cpart);
+		deallocate_2d_array(matrixA, sizeRowPerProcsA);
+		deallocate_2d_array(matrixB, sizeRowPerProcsB);
+		deallocate_2d_array(cpart, sizeRowPerPartC);
 	}
 
 	MPI_Finalize();
